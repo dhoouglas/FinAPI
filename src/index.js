12ab8844fe84a73eef4ac2b8fc582874e83a522c
Douglas Araujo <dhoouglas@hotmail.com>
2023-01-09 15:17:48 -0300

    Created method Post

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3333);
+
+const customers = [];
+
+app.post("/accounts", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const id = uuidv4();
+
+    customers.push({
+        cpf, name, id, statement: []
+    });
+
+    return response.status(201).send();
+    
+
+});
