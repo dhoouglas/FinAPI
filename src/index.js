71060bf966e05489c5584d411c14f2f84c160338
Douglas Araujo <dhoouglas@hotmail.com>
2023-01-09 16:48:48 -0300

    Middlewares VerifyIfExistsAccountCPF

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -9,6 +9,19 @@ app.use(express.json());
 app.listen(3333);
 
 const customers = [];
+
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+    if(!customer) {
+        return response.status(400).json({ error: "Customer not found" });
+    }
+
+    request.customer = customer;
+
+    return next();
+}
 
 app.post("/accounts", (request, response) => {
     const { cpf, name } = request.body;
@@ -27,4 +40,9 @@ app.post("/accounts", (request, response) => {
     return response.status(201).send();
     
 
+});
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    return response.json(customer.statement);
 });
